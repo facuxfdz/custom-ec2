ed77edfa04c659fe15d5361d2ec1fab979da2b79
Facundo Fernandez <facuassain@gmail.com>
2023-09-26 16:42:54 -0300

    terraform main backend in s3 bucket

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5"
     }
   }
+
+  backend "s3" {
+    bucket = "facuxfdz-bucket-1"
+    key = "terraform/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
